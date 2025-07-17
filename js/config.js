4d24ae571f7bc3da21b343e4d0e63092894667e7
marcniq15 <aniqpt01@gmail.com>
2025-07-17 11:39:52 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_nDjXG29fk', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 't6ef8fsgemh4u6dmjl1434s5fpu6tmmlbq86h1lk4uepcs4nbhg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3ah0uj4jngoacsacs4gueunoj3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
